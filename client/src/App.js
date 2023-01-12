60fdc27a51d5efff0abe07fdbf5b7041e159e20f
Tot3sMalot3s <bkatsevych@icloud.com>
2023-01-12 16:44:32 +0200

    apollo client instance

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import { Header } from "./components/Header";
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql",
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
-        <>
+        <ApolloProvider client={client}>
             <Header />
             <div className="container">hello world</div>
-        </>
+        </ApolloProvider>
     );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,10 @@
 import logo from "./assets/logo.png";
 
-export const Header = () => {
+export default function Header() {
     return (
         <nav className="navbar bg-light mb-4 p-0">
             <div className="container">
-                <a href="/" className="navbar-brand">
+                <a className="navbar-brand" href="/">
                     <div className="d-flex">
                         <img src={logo} alt="logo" className="mr-2" />
                         <div>ProjectMgmt</div>
@@ -13,4 +13,4 @@ export const Header = () => {
             </div>
         </nav>
     );
-};
+}
